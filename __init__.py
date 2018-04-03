9ae70caeae106fd839504d4b321f54989f33b112
Kyle Carretto <kcarretto@gmail.com>
2018-04-03 01:41:47 -0400

    Exported config API_KEY_FILE and TEAMSERVER_URI

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@
 This package contains objects and methods for interacting with the Arsenal teamserver API.
 """
 from .pyclient import Action, Group, GroupAction, Log, Session, Target
-from .pyclient import ArsenalClient
+from .pyclient import ArsenalClient, API_KEY_FILE, TEAMSERVER_URI
 
 from .cli import CLI
