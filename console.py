10a6efebb3f8f69868214b15f5c6c232a93dbbd3
Kyle Carretto <kcarretto@gmail.com>
2018-04-13 03:43:24 -0400

    Added interact built-in, allowing for a shell like experience with a target.

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 
 import sys
 import threading
+import time
 
 import fire
 
@@ -22,12 +23,20 @@ class Reset(Exception):
     """
     Raised when the user wishes to reinitialize the terminal.
     """
+    pass
+
+class ShellExit(Exception):
+    """
+    Raised when the user exits a shell.
+    """
+    pass
 
 class ArsenalCompleter(Completer): # pylint: disable-all
     """
     A completer specific to the Arsenal API.
     """
-    _api_methods = {}
+    _api_methods = []
+    _built_ins = []
     target_names = []
     group_names = []
     role_names = []
@@ -41,6 +50,8 @@ class ArsenalCompleter(Completer): # pylint: disable-all
         if '*' in self._api_methods:
             self._api_methods = list(filter(lambda x: not x.startswith('_'), dir(CLI)))
 
+        self._built_ins = ['help', 'interact', 'exit', 'reset']
+
         self.target_names = autocomplete.get('target_names', [])
         self.group_names = autocomplete.get('group_names', [])
         self.role_names = autocomplete.get('role_names', [])
@@ -48,6 +59,14 @@ class ArsenalCompleter(Completer): # pylint: disable-all
         self.user_names = autocomplete.get('user_names', [])
 
         self.auto_completers = {
+            'help': [
+                WordCompleter(self._api_methods)
+            ],
+            'interact': [
+                WordCompleter(self.target_names)
+            ],
+            'reset': [],
+            'exit': [],
             'GetTarget': [
                 WordCompleter(self.target_names)
             ],
@@ -97,7 +116,7 @@ class ArsenalCompleter(Completer): # pylint: disable-all
             ]
         }
 
-        self.api_completer = WordCompleter(self._api_methods, True)
+        self.api_completer = WordCompleter(list(set(self._api_methods+self._built_ins)), True)
 
     def get_completions(self, document, complete_event):
         """
@@ -105,7 +124,7 @@ class ArsenalCompleter(Completer): # pylint: disable-all
         """
         words = document.text.split(' ')
 
-        if words and words[0] in self._api_methods:
+        if words and (words[0] in self._api_methods or words[0] in self._built_ins):
             completers = self.auto_completers.get(words[0])
             if completers:
                 try:
@@ -142,6 +161,80 @@ class FireThread(threading.Thread):
     def run(self):
         fire.Fire(self.cli, '{}'.format(self._cmd))
 
+class Executor(threading.Thread):
+    """
+    Run CreateAction in a separate thread, waiting for the action to complete.
+    """
+    def __init__(self, client, target_name, cmd):
+        self.target_name = target_name
+        self.client = client
+        self.cmd = cmd
+        threading.Thread.__init__(self)
+
+    @handle_exceptions
+    def run(self):
+        action_id = self.client.create_action(
+            self.target_name, 'exec {}'.format(self.cmd), None, None, True)
+
+        resp = None
+
+        # Poll, waiting for action to complete
+        while not resp:
+            action = self.client.get_action(action_id)
+            if hasattr(action, 'response') and action.response:
+                resp = action.response
+                break
+            time.sleep(1)
+
+        print(resp.get('stdout', ''), file=sys.stdout)
+        print(resp.get('stderr', ''), file=sys.stderr)
+
+
+def shell(client, target_name):
+    """
+    Join an interactive shell for a target. Still uses the normal beacons, but automatically
+    adds the --quick flag to create action, and polls for output.
+    """
+    history = InMemoryHistory()
+
+    print('\nEntering into arsenal shell. \n \
+        Please note commands may take a while to execute, depending on session intervals. \n\n')
+
+    while True:
+        try:
+            text = prompt(
+                'Arsenal # ',
+                history=history,
+                auto_suggest=AutoSuggestFromHistory(),
+            )
+
+            if text.lower() == 'exit' or text.lower() == 'quit':
+                break
+
+            if text.strip() == '':
+                continue
+
+            background = False
+            if text.lower().startswith('&'):
+                text = text[1:]
+                background = True
+
+            executor = Executor(client, target_name, text)
+            executor.start()
+            if background:
+                print("Backgrounded job. Output will display once completed.")
+            else:
+                executor.join()
+
+        except EOFError:
+            break
+        except KeyboardInterrupt:
+            break
+        except Exception:
+            break
+
+    raise ShellExit()
+
 def exit_arsenal():
     """
     Exit the console
@@ -149,7 +242,7 @@ def exit_arsenal():
     print("\nThanks for using Arsenal")
     sys.exit(0)
 
-def parse_command(text):
+def parse_command(client, text):
     """
     Parse the input from the console.
     """
@@ -159,6 +252,12 @@ def parse_command(text):
         exit_arsenal()
     if cmd == 'reset':
         raise Reset()
+    if cmd.startswith('interact'):
+        tokens = cmd.split(' ')
+        if len(tokens) < 2:
+            print('usage: interact <target name>')
+            raise ShellExit()
+        shell(client, cmd.split(' ')[1])
     return text
 
 def build_autocomplete(client):
@@ -185,16 +284,19 @@ def build_autocomplete(client):
         except Exception as exception:
             print(type(exception))
             print(exception)
-    resp['target_names'] = [
-        target.name for target in safe_discover(client.list_targets, include_status=False)]
-    resp['group_names'] = [
-        group.name for group in safe_discover(client.list_groups)]
-    resp['role_names'] = [
-        role.name for role in safe_discover(client.list_roles)]
-    resp['agent_names'] = [
-        agent.name for agent in safe_discover(client.list_agents)]
-    resp['user_names'] = [
-        user.username for user in safe_discover(client.list_users)]
+    try:
+        resp['target_names'] = [
+            target.name for target in safe_discover(client.list_targets, include_status=False)]
+        resp['group_names'] = [
+            group.name for group in safe_discover(client.list_groups)]
+        resp['role_names'] = [
+            role.name for role in safe_discover(client.list_roles)]
+        resp['agent_names'] = [
+            agent.name for agent in safe_discover(client.list_agents)]
+        resp['user_names'] = [
+            user.username for user in safe_discover(client.list_users)]
+    except:
+        pass
 
     return resp
 
@@ -218,7 +320,7 @@ def main():
                 auto_suggest=AutoSuggestFromHistory()
             )
             if text:
-                text = parse_command(text)
+                text = parse_command(cli.client, text)
                 firethread = FireThread(text, cli)
                 firethread.start()
                 firethread.join()
@@ -227,6 +329,8 @@ def main():
             cli.client.context = cli.client.get_current_context()
             methods = cli.client.context.allowed_api_calls
             autocomplete = build_autocomplete(cli.client)
+        except ShellExit:
+            pass
         except EOFError:
             exit_arsenal()
         except KeyboardInterrupt:
